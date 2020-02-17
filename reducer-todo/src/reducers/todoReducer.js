5dffa0b08dffdf143248dfd37a827739fdea3952
Keyeric <keyeric.bristol@gmail.com>
2020-02-17 14:02:48 -0700

    created initialState export

--- reducer-todo/src/reducers/todoReducer.js
+++ reducer-todo/src/reducers/todoReducer.js
@@ -1,3 +1,9 @@
+export const initialState = {
+    item: "Learn about reducers",
+    completed: false,
+    id:62654986
+}
+
 export const todoReducer = (state, action) => {
     console.log(action);
     switch(action.type){
