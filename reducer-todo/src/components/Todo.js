4c5ab2d51113e87f18163b4edbe4d0da8f0c9551
Keyeric <keyeric.bristol@gmail.com>
2020-03-16 12:42:31 -0700

    added styling and tried to force quokka to work with react, did not succeed

--- reducer-todo/src/components/Todo.js
+++ reducer-todo/src/components/Todo.js
@@ -1,57 +1,87 @@
-import React, { useState, useReducer } from 'react';
+import React, { useState, useReducer } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 
 import { todoReducer, initialState } from "../reducers/todoReducer";
 
+const useStyles = makeStyles({
+  todo: {
+    display: "flex",
+    flexDirection: "column",
+    justfyContent: "center",
+    alignItems: "center",
+    background: "black",
+    margin: "5rem 25rem",
+    padding: "1rem 0",
+    border: "0.5rem solid red",
+    "& h1": {
+      color: "white"
+    },
+    "& div": {
+      // padding: "1rem 0",
+      margin: "0.5rem",
+      textAlign: "center"
+    }
+  }
+});
+
 export const Todo = () => {
-    const [state, dispatch] = useReducer(todoReducer, initialState);
-    console.log(state, dispatch);
-    const [newTaskName, setNewTaskName] = useState('');
+  const classes = useStyles();
+  const [state, dispatch] = useReducer(todoReducer, initialState);
+  console.log(state, dispatch);
+  const [newTaskName, setNewTaskName] = useState("");
 
   const handleChanges = e => {
     setNewTaskName(e.target.value);
   };
 
-    return (
-        <section>
-            <h1> To-Do List</h1>
-            <div>
-                <input
-                    className="task-input"
-                    type="text"
-                    name="newTaskName"
-                    value={newTaskName}
-                    onChange={handleChanges} 
-                />
-                <button 
-                onClick={() => {
-                    dispatch({ type: 'ADD_TASK', payload: newTaskName });
-                }}
-                > Add task
-                </button>
-            </div>
+  return (
+    <section className={classes.todo}>
+      <h1> To-Do List</h1>
+      <div>
+        <input
+          className="task-input"
+          type="text"
+          name="newTaskName"
+          value={newTaskName}
+          onChange={handleChanges}
+        />
+        <br />
+        <button
+          onClick={() => {
+            dispatch({ type: "ADD_TASK", payload: newTaskName });
+          }}
+        >
+          {" "}
+          Add task
+        </button>
+      </div>
 
-            <div>
-                {state.tasks.map(item => {
-                    console.log(item)
-                    return <div className = {item.completed ? "ToggleComplete" : "ToggleIncomplete" } 
-                    key = {item.id}
-                        onClick={() => {
-                            dispatch({ type: 'TOGGLE_COMPLETED', payload: item});
-                        }}>{item.item}
-                    </div>
-                    }
-                )}
+      <div>
+        {state.tasks.map(item => {
+          console.log(item);
+          return (
+            <div
+              className={item.completed ? "ToggleComplete" : "ToggleIncomplete"}
+              key={item.id}
+              onClick={() => {
+                dispatch({ type: "TOGGLE_COMPLETED", payload: item });
+              }}
+            >
+              {item.item}
             </div>
+          );
+        })}
+      </div>
 
-            <div>
-                <button 
-                    onClick={() => {
-                        dispatch({ type: 'REMOVE_COMPLETED'});
-                    }}
-                    > 
-                    Clear Finished
-                </button>
-            </div>
-        </section>
-    )
-}
+      <div>
+        <button
+          onClick={() => {
+            dispatch({ type: "REMOVE_COMPLETED" });
+          }}
+        >
+          Clear Finished
+        </button>
+      </div>
+    </section>
+  );
+};
--- reducer-todo/src/reducers/todoReducer.js
+++ reducer-todo/src/reducers/todoReducer.js
@@ -1,40 +1,57 @@
 export const initialState = {
-    tasks:[
-        {
-            item: "Learn about reducers",
-            completed: false,
-            id:62654986
-        },
-    ]
-}
+  tasks: [
+    {
+      item: "Learn about reducers",
+      completed: false,
+      id: 62654986
+    },
+    {
+      item: "Hate redux",
+      completed: false,
+      id: 97359735
+    },
+    {
+      item: "Learn redux",
+      completed: false,
+      id: 9742569524
+    },
+    {
+      item: "Brag",
+      completed: false,
+      id: 9524792579
+    }
+  ]
+};
 
 export const todoReducer = (state, action) => {
-    console.log(action);
-    switch(action.type){
-        case "ADD_TASK":
-            const newTask = {item: action.payload, completed: false, id: Date.now()}
-            return{...state, 
-                tasks: [...state.tasks, newTask ]}
-            case "TOGGLE_COMPLETED":
-            return{...state,
-                tasks: state.tasks.map(item => {
-                    if (item.id === action.payload.id){
-                        return{...item, 
-                            completed: !item.completed} 
-                    } else {
-                        return item
-                    }
-                })
-            }
-            case "REMOVE_COMPLETED":
-                return {
-                    ...state, 
-                    tasks: state.tasks.filter((task)=>
-                        {
-                            return !task.completed;
-                        })
-                }
-        default:
-            return state;
-    }
-}
+  console.log(action);
+  switch (action.type) {
+    case "ADD_TASK":
+      const newTask = {
+        item: action.payload,
+        completed: false,
+        id: Date.now()
+      };
+      return { ...state, tasks: [...state.tasks, newTask] };
+    case "TOGGLE_COMPLETED":
+      return {
+        ...state,
+        tasks: state.tasks.map(item => {
+          if (item.id === action.payload.id) {
+            return { ...item, completed: !item.completed };
+          } else {
+            return item;
+          }
+        })
+      };
+    case "REMOVE_COMPLETED":
+      return {
+        ...state,
+        tasks: state.tasks.filter(task => {
+          return !task.completed;
+        })
+      };
+    default:
+      return state;
+  }
+};
